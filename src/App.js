6d1c5389be3ac36f1b1de4ac0e37d67546bfdb20
Abin John <15942221+abinjohn123@users.noreply.github.com>
2023-02-22 20:09:21 +0530

    feat: create slogan search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
