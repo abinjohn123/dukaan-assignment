ab813fc9abca46ca75e4ac2812a38af7dbbb848d
Abin John <15942221+abinjohn123@users.noreply.github.com>
2023-02-22 19:00:29 +0530

    feat: create header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import Header from "./Header";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <div className="header-container">
+      <header className="header">
+        <a href="https://mydukaan.io/">
+          <img src="assets/logo/dukaan-white.svg" alt="Dukaan logo"></img>
+        </a>
+        <div className="header-right">
+          <a href="#" className="header-link-blank">
+            Sign in
+          </a>
+          <a href="#" className="header-link-white">
+            Dukaan for PC
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
