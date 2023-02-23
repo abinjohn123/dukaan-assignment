abeb8026f5e3fe586f9876d42573ac30d6669809
Abin John <15942221+abinjohn123@users.noreply.github.com>
2023-02-23 14:10:32 +0530

    refactor: remove unwanted container div from header

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,17 @@
 export default function Header() {
   return (
-    <div className="header-container side-padding">
-      <header className="header">
-        <a href="https://mydukaan.io/">
-          <img src="assets/logo/dukaan-white.svg" alt="Dukaan logo"></img>
+    <header className="header side-padding">
+      <a href="https://mydukaan.io/">
+        <img src="assets/logo/dukaan-white.svg" alt="Dukaan logo"></img>
+      </a>
+      <div className="header-right">
+        <a href="#" className="header-link-blank">
+          Sign in
         </a>
-        <div className="header-right">
-          <a href="#" className="header-link-blank">
-            Sign in
-          </a>
-          <a href="#" className="header-link-white">
-            Dukaan for PC
-          </a>
-        </div>
-      </header>
-    </div>
+        <a href="#" className="header-link-white">
+          Dukaan for PC
+        </a>
+      </div>
+    </header>
   );
 }
